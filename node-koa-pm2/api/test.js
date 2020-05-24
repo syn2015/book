41c1a3e690bcc2383518afdda8d99d689504fbbc
syn2015 <syn201509@163.com>
2020-05-24 21:19:22 +0800

    node-koa-pm2 init

---
+++ node-koa-pm2/api/test.js
@@ -0,0 +1,21 @@
+const Router=require('koa-router')
+
+const router=new Router();
+router
+    .get('/api',(ctx,next)=>{
+        ctx.body='hello world';
+    })
+    .get('api/test',(ctx,next)=>{
+        ctx.body={
+            msg:'here is test',
+            query:ctx.query,
+            queryStr:ctx.querystring
+        }
+    })
+    .post('/api/users',(ctx,next)=>{
+        ctx.body='here is users'
+    })
+    .all('/api/users/:id',(ctx,next)=>{
+        //...
+    })
+    module.exports=router
---
+++ node-koa-pm2/app.js
@@ -0,0 +1,12 @@
+const koa=require('koa');
+const app=new koa();
+const router=require('./api/test')
+app.use(async(ctx,next)=>{
+    const start=Date.now();
+    await next();
+    const ms=Date.now()-start;
+    console.log(`${ctx.method} ${ctx.url}-${ms}ms`);
+
+})
+app.use(router.routes());
+app.listen(3000);
