4640d1cd70e2c85fb99e2c6391f838c788fe7c33
syn2015 <syn201509@163.com>
2020-08-27 15:21:25 +0800

    html5 game

--- html5_game_nervecat/app.js
+++ html5_game_nervecat/app.js
@@ -1,12 +1,26 @@
+// 创建stage对象
 var stage=new createjs.Stage("gameView");
+// 创建容器
 var gameView=new createjs.Container();
-stage.addChild(gameView);
-
-var s=new createjs.Shape();
-s.graphics.beginFill('#ff0000');
-s.graphics.drawCircle(50,50,25);
-s.graphics.endFill();
-gameView.addChild(s);
+stage.gameView
 
 createjs.Ticker.setFPS(30);
 createjs.Ticker.addEventListener('tick',stage);
+
+//
+// var stage=new createjs.Stage("gameView");
+// var gameView=new createjs.Container();
+// // 添加容器gameView
+// stage.addChild(gameView);
+
+// var s=new createjs.Shape();
+// s.graphics.beginFill('#ff0000');
+// s.graphics.drawCircle(50,50,25);
+// // 结束绘制
+// s.graphics.endFill();
+// // 添加到容器中
+// gameView.addChild(s);
+
+// createjs.Ticker.setFPS(30);
+// 监听事件
+// createjs.Ticker.addEventListener('tick',stage);
