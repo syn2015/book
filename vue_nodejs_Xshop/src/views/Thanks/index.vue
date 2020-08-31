19fe5f908f412a4ca1a935290afe999619a0ec46
syn2015 <syn201509@163.com>
2020-08-31 22:15:07 +0800

    src/

---
+++ vue_nodejs_Xshop/src/views/Thanks/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Thanks</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Thanks"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
