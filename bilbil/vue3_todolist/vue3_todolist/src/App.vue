3f2f4bcdefca9426b37197fe07c13f92fa721a4e
syn2015 <syn201509@163.com>
2020-10-13 14:09:36 +0800

    todolist 拆分组件

--- bilbil/vue3_todolist/vue3_todolist/src/App.vue
+++ bilbil/vue3_todolist/vue3_todolist/src/App.vue
@@ -2,61 +2,55 @@
   <main>
     <div class="container">
       <h1>Sun 代办事项！</h1>
-      <div class="input-add">
-        <input type="text" name="todo">
-        <button>
-          <i class="plus"></i>
-        </button>
-      </div>
-      <div class="filters">
-        <span class="filter active">全部</span>
-        <span class="filter">已完成</span>
-        <span class="filter">未完成</span>
-      </div>
-      <div class="todo-list">
-        <div class="todo-item">
-          <label for="">
-            <input type="checkbox">
-            todo1
-            <span class="check-button"></span>
-          </label>
-        </div>
-          <div class="todo-item">
-          <label for="">
-            <input type="checkbox">
-            todo1
-            <span class="check-button"></span>
-          </label>
-        </div>
-          <div class="todo-item">
-          <label for="">
-            <input type="checkbox">
-            todo1
-            <span class="check-button"></span>
-          </label>
-        </div>
-      </div>
+      <todo-add />
+      <todo-filter />
+      <todo-list />
     </div>
   </main>
 </template>
 
 <script>
-
+import TodoAdd from './components/TodoAdd'
+import TodoFilter from './components/TodoFilter'
+import TodoList from './components/TodoList'
 export default {
   name: 'App',
   components: {
-
+    TodoAdd,
+    TodoFilter,
+    TodoList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+main {
+  width: 100vw;
+  /* 保持背景容器到滚动条底部 */
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  background: rgb(203, 210, 240);
+}
+.container {
+  max-width: 400px;
+  /* width: 60%; */
+
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background: rgb(245, 246, 252);
+}
+h1 {
+  margin: 24px 0;
+  font-size: 28px;
+  color: #414873;
 }
 </style>
---
+++ bilbil/vue3_todolist/vue3_todolist/src/components/TodoListItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="todo-item">
+    <label>
+      <input type="checkbox" />
+      todo1
+      <span class="check-button"></span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem'
+}
+</script>
+
+<style >
+.todo-item {
+  background: white;
+  padding: 16px;
+  border-radius: 8px;
+  color: #626262;
+}
+.todo-item label {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.todo-item label span.check-button {
+  position: absolute;
+  top: 0;
+}
+/* 空心 */
+.todo-item label span.check-button::before {
+  border: 1px solid #b382f9;
+}
+/* 实心 */
+.todo-item label span.check-button::after {
+  transition: 0.4s;
+  background: #b382f9;
+  /* 向下移动1px */
+  transform: translate(1px, 1px) scale(0.8);
+  opacity: 0;
+}
+/* 选中和未选中通用代码 */
+.todo-item label span.check-button::before,
+.todo-item label span.check-button::after {
+  content: " ";
+  display: block;
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+}
+/* 隐藏input */
+.todo-item input {
+  margin-right: 16px;
+  opacity: 0;
+}
+/* 实现选中状态 */
+.todo-item input:checked + span.check-button::after {
+  opacity: 1;
+}
+</style>
